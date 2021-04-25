1c48c69ebcb6e3f82ef32fc1547df72c70e9e888
crazykazu <63088568+crazykazu@users.noreply.github.com>
2021-04-25 13:23:38 -0700

    First edit to app.py
    Just seeing how to work this github

--- web/app.py
+++ web/app.py
@@ -1,14 +1,29 @@
 """
-John Doe's Flask API.
+Kazu Umemoto's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, render_template, send_from_directory, request, abort
 
 app = Flask(__name__)
+
+@app.route("/<path:page>")
+def open(page):
+    if ('//' in page) or ('~' in page) or ('..' in page):
+        abort(403)
+    else:
+        return render_template(page)
 
 @app.route("/")
 def hello():
     return "UOCIS docker demo!\n"
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def file_forbidden(e):
+    return render_template('403.html'), 403
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
