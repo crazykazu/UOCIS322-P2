82ea2a410f25205a1e23917ba416a0335c394b06
crazykazu <63088568+crazykazu@users.noreply.github.com>
2021-04-25 15:12:10 -0700

    adding the send from directory

--- web/app.py
+++ web/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def open(page):
     if ('//' in page) or ('~' in page) or ('..' in page):
         abort(403)
+    return send_from_directory('pages')
 
 @app.errorhandler(404)
 def not_found(e):
